dd002b28743ea5d82cf46094e85f78688837d896
Bochkovskyi <unundertale1@ukr.net>
2023-07-04 17:02:47 +0300

    add searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
+import SearchBar from "./components/UI/SearchBar";
 import TodoLists from "./components/Todo/TodoLists";
 import TodoListsContext from "./store/todo-lists-context";
 import "./App.scss";
@@ -12,6 +13,7 @@ function App() {
     <>
       <CssBaseline />
       <Container>
+        <SearchBar label="Search Todo" />
         {isLoading ? <p>Loading...</p> : <TodoLists />}
         {error && <p>{error}</p>}
       </Container>
---
+++ src/components/UI/SearchBar.jsx
@@ -0,0 +1,33 @@
+import { useRef } from "react";
+import Box from "@mui/material/Box";
+import Input from "@mui/material/Input";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+
+export default function SearchBar({ label }) {
+  const searchInputRef = useRef(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(searchInputRef.current.value);
+    searchInputRef.current.value = "";
+  };
+
+  return (
+    <Box
+      component="form"
+      sx={{ display: "flex", alignItems: "center" }}
+      onSubmit={handleSubmit}
+    >
+      <Input
+        fullWidth
+        variant="standard"
+        placeholder={label}
+        inputRef={searchInputRef}
+      />
+      <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+    </Box>
+  );
+}
