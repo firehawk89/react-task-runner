d14d9a52f438829c90ec9583df2493c6c9310589
Bochkovskyi <unundertale1@ukr.net>
2023-07-04 16:36:20 +0300

    add default styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { useContext } from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import TodoListsContext from "./store/todo-lists-context";
 import "./App.scss";
 
 function App() {
-  return <></>;
+  const { isLoading, error } = useContext(TodoListsContext);
+
+  return (
+    <>
+      <CssBaseline />
+      <Container>
+        {isLoading ? <p>Loading...</p> : <div>Data Successfully Loaded!</div>}
+        {error && <p>{error}</p>}
+      </Container>
+    </>
+  );
 }
 
 export default App;
